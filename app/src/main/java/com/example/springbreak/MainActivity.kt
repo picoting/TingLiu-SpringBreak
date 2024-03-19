2ef1b70d0f91f0c9fdc24dbb71fb1e122c61d31a
picoting <lindsayl.6857@gmail.com>
2024-03-18 22:17:52 -0400

    speech to text works git add .

--- app/src/main/java/com/example/springbreak/MainActivity.kt
+++ app/src/main/java/com/example/springbreak/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.springbreak
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
-
+import android.speech.RecognizerIntent
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var listView: ListView
     private lateinit var editText: EditText
 
+    private val speechRequestCode = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,8 +41,45 @@ class MainActivity : AppCompatActivity() {
             val cities = languageCitiesMap[language]
             val selectedCity = cities?.get(Random.nextInt(cities.size)) ?: ""
 
-            editText?.setText("$language - $selectedCity")
+            startSpeechToText(language)
+
+            //editText.setText("$language - $selectedCity")
         }
 
+    }
+
+    private fun startSpeechToText(selectedLanguage: String) {
+        val locale = when (selectedLanguage) {
+            "English" -> "en-US"
+            "French" -> "fr-FR"
+            "Chinese" -> "zh-CN"
+            else -> "en-US" // default or error handling
+        }
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, locale)
+            putExtra(RecognizerIntent.EXTRA_PROMPT, "Please speak now")
+        }
+        try {
+            startActivityForResult(intent, speechRequestCode)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Your device does not support Speech Input", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == speechRequestCode && resultCode == RESULT_OK) {
+            val spokenText: String? =
+                data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.let { results ->
+                    results[0]
+                }
+
+            spokenText?.let {
+                editText.setText(it)
+            }
+        }
     }
 }
